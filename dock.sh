7016c2688cc26aa5af3d671f8c821efae708c54a
amassaad <alex.massaad@shopify.com>
2014-12-14 10:31:53 -0500

    Finally works, this fixes #1

--- dock.sh
+++ dock.sh
@@ -4,7 +4,8 @@ if [ ! -f Dockerfile ];
   then
   echo "No Dockerfile yet"
   cp Dockerfile.example Dockerfile
-  sed -i s/REPLACE_ME_NOW\!/$(bundle exec rake secret)/ /Users/admin/Dropbox/code/sms-panda/Dockerfile
+
+  sed -i '' "s/REPLACE_ME_NOW/$(bundle exec rake secret)/" /Users/admin/Dropbox/code/sms-panda/Dockerfile
 else
   echo "We have docking!"
 fi
