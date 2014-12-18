1a3005963b89361da33bf57fbb69cbfdde753a64
amassaad <alex.massaad@shopify.com>
2014-12-17 22:12:12 -0500

    Update

--- codeship.sh
+++ codeship.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-mkdir -p /home/ubuntu/current
-sudo chmod -R 757 /home/ubuntu/current
-cd /home/ubuntu/current
-pwd
-sudo apt-get update
-sudo apt-get -y install git
-git clone https://github.com/ATAMProductions/sms-panda.git
+# mkdir -p /home/ubuntu/current
+# sudo chmod -R 757 /home/ubuntu/current
+# cd /home/ubuntu/current
+# pwd
+# sudo apt-get update
+# sudo apt-get -y install git
+# git clone https://github.com/ATAMProductions/sms-panda.git
 sudo curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` >  /usr/local/bin/fig'
 sudo chmod +x /usr/local/bin/fig
 cd /home/ubuntu/current/sms-panda
