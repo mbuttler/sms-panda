957ec10bf3b5a8dc81c1fc96f6c3bdbbccad6bc2
amassaad <alex.massaad@shopify.com>
2014-12-18 17:56:14 -0500

    Update the script?

--- codeship.sh
+++ codeship.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` >  /usr/local/bin/fig
-sudo chmod +x /usr/local/bin/fig
+curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` >  /usr/local/bin/fig
+chmod +x /usr/local/bin/fig
 cd /home/ubuntu/current/sms-panda
-sudo fig build
-sudo fig run web rake db:create
-sudo fig run web rake db:migrate
-sudo fig up -d
+fig build
+fig run web rake db:create
+fig run web rake db:migrate
+fig up -d
